92511c7c7a641f3a50d4b282375424c1694b475f
Rose <mehrabkhanrose10@gmail.com>
2024-09-18 18:32:29 +0600

    first commit

---
+++ Current date and time.py
@@ -0,0 +1,15 @@
+from datetime import date, time, datetime
+
+today = date.today()
+print(today)
+
+now = datetime.now()
+print(now)
+print()
+
+print(f"Current Year : {today.year}")
+print(f"Current Month : {today.month}")
+print(f"Current Day : {today.day}")
+
+print()
+
